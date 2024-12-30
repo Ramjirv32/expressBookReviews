cab0f50b52e6077a34fbf3cd3b36c9621c0b33b7
ramji <ramjib2311@gmail.com>
2024-12-30 20:14:44 +0530

    added boolean

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,13 +5,18 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
+const isValid = (username)=>{ 
+  if(!username){
+    if(users.find(i=>i.username===username)){
+      return true;
+    }
+  }
+  return false;
+  
+  //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
